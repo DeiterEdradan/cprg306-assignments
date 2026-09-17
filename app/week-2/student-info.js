954729bf72dbafd20d3d8237b640a94fe545a699
DeiterEdradan <Deiter.Edradan@edu.sait.ca>
2026-09-17 13:16:14 -0600

    Update student-info.js

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -3,7 +3,7 @@ export default function StudentInfo() {
     return (
         <main>
             <p>Deiter Edradan</p>
-            <Link href="https://github.com/DeiterEdradan/cprg306-assignments">GitHub repository</Link>
+            <Link href="https://github.com/DeiterEdradan/cprg306-assignments">Link to GitHub repository</Link>
         </main>
     );
 }
