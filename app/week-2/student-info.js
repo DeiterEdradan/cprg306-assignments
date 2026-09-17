6685a35e110770fed0710a814e34d49ac2ccac45
DeiterEdradan <Deiter.Edradan@edu.sait.ca>
2026-09-17 12:51:58 -0600

    Update student-info.js
    created component to display name and github repository

---
+++ app/week-2/student-info.js
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+export default function StudentInfo() {
+    return (
+        <main>
+            <p>Deiter Edradan</p>
+            <Link href="https://github.com/DeiterEdradan/cprg306-assignments">GitHub repository</Link>
+        </main>
+    );
+}
