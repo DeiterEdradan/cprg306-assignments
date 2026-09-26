8736a9fbbe503fd637b8db61213017f366a06ab4
Jabinunger <Jabin.unger@gmail.com>
2026-09-25 18:09:01 -0600

    Complete Week 3 assignment

---
+++ app/week-3/page.js
@@ -0,0 +1,8 @@
+import ItemList from "./item-list";
+export default function Page() {
+
+return (
+    <main className="min-h-screen bg-slate-900 text-white p-6">
+      <h1 className="text-4xl font-bold m-6 text-center text-yellow-300">
+        Shopping List
+      </h1> <ItemList /> </main>); }
